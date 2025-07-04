d9bd549233b925c704235e5b0946cd3f8c4dd8cd
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2025-07-05 03:50:16 +0600

    \"fix: Resolve NextAuth session.user possibly undefined error\"

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -36,7 +36,9 @@ const handler = NextAuth({
       return token;
     },
     async session({ session, token }) {
-      session.user.id = token.id;
+      if (session.user) {
+        session.user.id = token.id;
+      }
       return session;
     },
   },
