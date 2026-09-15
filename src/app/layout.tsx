d44d01418e95f0b4f4947bb5e181a0f74a5a087a
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2026-09-15 15:32:52 +0600

    feat: updated icons and added manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,6 +50,17 @@ export const metadata: Metadata = {
   alternates: {
     canonical: "https://zahidhasantonmoy.vercel.app", // Replace with your actual domain
   },
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png' }
+    ],
+  },
+  manifest: "/site.webmanifest",
 };
 
 import Providers from "./providers";
